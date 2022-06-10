11d4909e50a9a02bdc299f90257d5a775421dcf2
Aarav Sharma <83349596+aaravshar@users.noreply.github.com>
2022-06-10 18:43:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function fun() {
+ alert(
+  "here is your cookie:- 🍪"
+ );
+}
+
+function sup() {
+ a = prompt(
+  "type your address we will deliver the secret gift 🤫🎁 to you door steps",
+  "address");
+ document.getElementById(
+   "ab").innerHTML =
+  "on" + " " + a + " " +
+  "we will deliver the secret gift 🤫🎁.";
+ console.log(a);
+}
